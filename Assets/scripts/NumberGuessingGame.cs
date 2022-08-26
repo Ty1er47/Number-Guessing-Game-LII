88abbb2a32e3abe07d8c775b38d5b444156fe5dc
Ty1er47 <111007514+Ty1er47@users.noreply.github.com>
2022-08-26 11:28:31 -0700

    Copied from old game

--- Assets/scripts/NumberGuessingGame.cs
+++ Assets/scripts/NumberGuessingGame.cs
@@ -7,7 +7,42 @@ public class NumberGuessingGame : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        {
+    private int guess = 5;
+    private int minValue = 1;
+    private int maxValue = 10;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+    StartGame();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       if(Input.GetKeyDown(KeyCode.UpArrow))
+       {
+         minValue = guess;
+         guess = (maxValue + minValue) / 2;
+         Debug.Log("Is it higher or lower than: " + guess);
+       } 
+       if(Input.GetKeyDown(KeyCode.DownArrow))
+       {
+           maxValue = guess;
+           guess = (maxValue + minValue) /2;
+           Debug.Log("Is it higher or lower than: " + guess);
+        }
+          if(Input.GetKeyDown(KeyCode.Return)) 
+        {
+          Debug.Log("I guessed your number,I'm a genius!!");
+          Debug.Log("");
+          StartGame();
+        }
+    }
+
+     void StartGame () 
+    {
     }
 
     // Update is called once per frame
